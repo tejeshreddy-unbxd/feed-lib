59366e3aa3bf88adc42a0dd170791fe28d0851e1
unbxd <unbxd@Tejeshs-MacBook-Pro.local>
2019-10-01 15:06:54 +0530

    File deleted

--- includes/config.py
+++
@@ -1,5 +0,0 @@
-'''
-    - This file holds transformations to be included in the feed.py file
-    - Any number of transformation can be included to this file
-'''
-
--- includes/fieldhelpers.py
+++
@@ -1,4 +0,0 @@
-'''
-    - Individual isolated functions to be added here 
-    - Client specific functions to be added along with generic functions
-'''
